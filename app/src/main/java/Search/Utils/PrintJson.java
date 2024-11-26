bf2a195fdda8a6d9ba7c0871fbd40d4af671e6c7
Shabrina Alya Aureliya <165859494+deseafiles@users.noreply.github.com>
2024-11-26 23:08:26 +0800

    makasi min

--- app/src/main/java/Search/Utils/PrintJson.java
+++ app/src/main/java/Search/Utils/PrintJson.java
@@ -7,14 +7,11 @@ import Search.Models.rbTree;
 
 public class PrintJson {
     public static List<String> printRedBlackTreeResults(rbTree<String, String> rbTree, String searchParam) {
-        // Memanggil search pada rbTree untuk mendapatkan hasil pencarian
         List<Node<String, String>> result = rbTree.search(searchParam);
         ArrayList<String> combinedResults = new ArrayList<>();
 
         if (result != null && !result.isEmpty()) {
-            // Mengonversi hasil Node ke dalam format String dan menambahkannya ke combinedResults
             for (Node<String, String> node : result) {
-                // Menambahkan key dan value dari node ke dalam combinedResults sebagai string
                 String resultString = "Key: " + node.getKey() + ", Value: " + node.getValue();
                 if (!combinedResults.contains(resultString)) {
                     combinedResults.add(resultString);
